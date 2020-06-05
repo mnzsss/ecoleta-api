b425b01fc8735bdb351eb5d3392bdac1e080d4e2
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-06-05 17:56:32 -0300

    feat: validate data

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi } from 'celebrate';
 import multer from 'multer';
 
 import uploadConfig from './config/upload';
@@ -11,7 +12,23 @@ const upload = multer(uploadConfig);
 
 routes.get('/items', ItemsController.index);
 
-routes.post('/points', upload.single('image'), PointsController.create);
+routes.post(
+  '/points',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    }),
+  }),
+  upload.single('image'),
+  PointsController.create,
+);
 routes.get('/points/:id', PointsController.show);
 routes.get('/points', PointsController.index);
 
